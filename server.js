9958f193aa8eb4cd994a3102bdb17cc981e59edf
Nabila Ayaba <nabilaayaba@gmail.com>
2024-10-23 10:20:14 +0300

    Add initial styling

--- server.js
+++ server.js
@@ -1,49 +1,52 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-const session = require('express-session');
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const morgan = require('morgan')
+const session = require('express-session')
+const path = require('path')
 
-const authController = require('./controllers/auth.js');
+const authController = require('./controllers/auth.js')
 
-const port = process.env.PORT ? process.env.PORT : '3000';
+const port = process.env.PORT ? process.env.PORT : '3000'
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI)
 
 mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+})
 
-app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
   })
-);
+)
 
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
-  });
-});
+  })
+})
 
 app.get('/vip-lounge', (req, res) => {
   if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
+    res.send(`Welcome to the party ${req.session.user.username}.`)
   } else {
-    res.send('Sorry, no guests allowed.');
+    res.send('Sorry, no guests allowed.')
   }
-});
+})
 
-app.use('/auth', authController);
+app.use('/auth', authController)
 
 app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`);
-});
+  console.log(`The express app is ready on port ${port}!`)
+})
