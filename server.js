6c54eeb9bb9f35ede3f36cdfe663880a6ba2667b
Nabila Ayaba <nabilaayaba@gmail.com>
2024-10-23 10:50:19 +0300

    Add authentication middleware

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const morgan = require('morgan')
 const session = require('express-session')
 const path = require('path')
 
-// CONTROLLER IMPORTS
+// CONTROLLER + MIDDLEWARE IMPORTS
+const isSignedIn = require('./middleware/is-signed-in.js')
+const passUserToView = require('./middleware/pass-user-to-view.js')
 const authController = require('./controllers/auth.js')
 const listingsController = require('./controllers/listings.js')
 
@@ -33,6 +35,8 @@ app.use(
   })
 )
 
+app.use(passUserToView)
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
@@ -48,7 +52,7 @@ app.get('/vip-lounge', (req, res) => {
 })
 
 app.use('/auth', authController)
-app.use('/listings', listingsController)
+app.use('/listings', isSignedIn, listingsController)
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`)
