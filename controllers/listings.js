1d53f8c8e8fca76f2767260be8222ed73eb826ca
Nabila Ayaba <nabilaayaba@gmail.com>
2024-10-23 14:40:08 +0300

    Create show page and route

--- controllers/listings.js
+++ controllers/listings.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const Listing = require('../models/listing')
 
+// render a list of all the listings (READ ALL)
 router.get('/', async (req, res) => {
     try {
         const populatedListings = await Listing.find({}).populate('owner')
@@ -16,14 +17,29 @@ router.get('/', async (req, res) => {
     }
 })
 
+// render a new listing form (CREATE part 1)
 router.get('/new', (req, res) => {
     res.render('listings/new.ejs')
 })
 
+// create (submit new listing form) (CREATE part 2)
 router.post('/', async (req, res) => {
     req.body.owner = req.session.user._id
     await Listing.create(req.body)
     res.redirect('/listings')
+})
+
+// show route (READ ONE)
+router.get('/:listingId', async (req, res) => {
+    try {
+        const populatedListing = await Listing.findById(req.params.listingId).populate('owner')
+        res.render('listings/show.ejs', {
+            listing: populatedListing
+        })
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
 })
 
 
