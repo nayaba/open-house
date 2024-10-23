e8876aedd659f3f6eaa4ca078fc52533064afa18
Nabila Ayaba <nabilaayaba@gmail.com>
2024-10-23 13:55:06 +0300

    Implement create functionality

--- controllers/listings.js
+++ controllers/listings.js
@@ -17,4 +17,11 @@ router.get('/new', (req, res) => {
     res.render('listings/new.ejs')
 })
 
+router.post('/', async (req, res) => {
+    req.body.owner = req.session.user._id
+    await Listing.create(req.body)
+    res.redirect('/listings')
+})
+
+
 module.exports = router
